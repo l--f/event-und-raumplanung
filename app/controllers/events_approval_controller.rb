6912940843703beffca9f9fff2152d997cd4f364
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2014-11-25 16:12:18 +0100

    added date selection at events_approval

--- app/controllers/events_approval_controller.rb
+++ app/controllers/events_approval_controller.rb
@@ -3,7 +3,11 @@ class EventsApprovalController < ApplicationController
 	before_action :read_and_exec_params
 	def read_and_exec_params
 		if params[:date]
-			@date = params[:date].to_date
+			if params[:date].is_a?(Hash)
+				@date = (params[:date][:year]+ '-' + params[:date][:month] + '-' + params[:date][:day]).to_date
+			else
+				@date = params[:date].to_date
+			end
 		else	
 			@date = Date.today
 		end
